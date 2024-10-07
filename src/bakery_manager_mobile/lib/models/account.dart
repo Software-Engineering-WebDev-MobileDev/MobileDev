bb76cb685c46ef7a7d574ef97624bc7f0bbaf4e9
Kashaina Nucum <knucum43@tntech.edu>
2024-10-06 22:15:42 -0500

    create account class

--- src/bakery_manager_mobile/lib/models/account.dart
+++ src/bakery_manager_mobile/lib/models/account.dart
@@ -1,9 +1,9 @@
 class Account {
-  final String employeeId;
-  final String firstName;
-  final String lastName;
-  final String username;
-  final String role;
+  String employeeId;
+  String firstName;
+  String lastName;
+  String username;
+  String role;
 
   Account({
     required this.employeeId,
@@ -13,25 +13,13 @@ class Account {
     required this.role,
   });
 
-  // Factory constructor to create an Account instance from JSON data
   factory Account.fromJson(Map<String, dynamic> json) {
     return Account(
       employeeId: json['EmployeeID'] ?? '',
       firstName: json['FirstName'] ?? '',
       lastName: json['LastName'] ?? '',
       username: json['Username'] ?? '',
-      role: json['Role'] ?? '',
+      role: json['Role'] ?? 'Employee',
     );
-  }
-
-  // Method to convert an Account instance to a JSON map
-  Map<String, dynamic> toJson() {
-    return {
-      'EmployeeID': employeeId,
-      'FirstName': firstName,
-      'LastName': lastName,
-      'Username': username,
-      'Role': role,
-    };
   }
 }
